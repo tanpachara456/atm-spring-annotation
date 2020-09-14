af0a0b53c845e28547221054ed8ede0a501268ea
tanpachara <tanpacharahsnl@gmail.com>
2020-09-14 22:01:37 +0700

    first commit

---
+++ src/main/java/atm/Main.java
@@ -0,0 +1,19 @@
+package th.ac.ku.atm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean.xml");
+        ATM atm = context.getBean(ATM.class);
+
+        String name = atm.validateCustomer(2,2345);
+        System.out.println( name + "has" + atm.getBalance());
+        atm.withdraw(400);
+        System.out.println( name + "has" + atm.getBalance());
+
+
+    }
+}
